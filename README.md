a7c51dae1f28921c3da1c18c18bc5f9fcaaf9ef3
Nguyen Ngoc Hung <hungnn@ahamove.com>
2020-11-09 09:24:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# HTTP Metabase Driver
+
+This is a proof-of-concept HTTP "driver" for [Metabase](https://metabase.com/).
+
+Previous discussion: https://github.com/metabase/metabase/pull/7047
+
+## Usage
+
+Currently the simplest "native" query for this driver is simply an object with a `url` property:
+
+```json
+{ "url": "https://api.coinmarketcap.com/v1/ticker/" }
+```
+
+The driver will make a `GET` request and parse the resulting JSON array into rows. Currently it only supports JSON.
+
+You can provide a different `method` as well as `headers` and a JSON `body`:
+
+```json
+{
+  "url": "https://api.coinmarketcap.com/v1/ticker/",
+  "method": "POST",
+  "headers": {
+    "Authentication": "SOMETOKEN"
+  },
+  "body": {
+    "foo": "bar"
+  }
+}
+```
+
+Additionally, you can provide a `result` object with a JSONPath to the "root" in the response, and/or a list of `fields`:
+
+```json
+{
+  "url": "https://blockchain.info/blocks?format=json",
+  "result": {
+    "path": "blocks",
+    "fields": ["height", "time"]
+  }
+}
+```
+
+You can also predefine "tables" in the database configuration's `Table Definitions` setting. These tables will appear in the graphical query builder:
+
+```json
+{
+  "tables": [
+    {
+      "name": "Blocks",
+      "url": "https://blockchain.info/blocks?format=json",
+      "fields": [
+        { "name": "height", "type": "number" },
+        { "name": "hash", "type": "string" },
+        { "name": "time", "type": "number" },
+        { "type": "boolean", "name": "main_chain" }
+      ],
+      "result": {
+        "path": "blocks"
+      }
+    }
+  ]
+}
+```
+
+There is limited support for aggregations and breakouts, but this is very experimental and may be removed in future versions.
+
+## Building the driver
+
+### Prereq: Install Metabase as a local maven dependency, compiled for building drivers
+
+Clone the [Metabase repo](https://github.com/metabase/metabase) first if you haven't already done so.
+
+```bash
+cd /path/to/metabase_source
+lein install-for-building-drivers
+```
+
+### Build the HTTP driver
+
+```bash
+# (In the HTTP driver directory)
+lein clean
+DEBUG=1 LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar
+```
+
+### Copy it to your plugins dir and restart Metabase
+
+```bash
+mkdir -p /path/to/metabase/plugins/
+cp target/uberjar/http.metabase-driver.jar /path/to/metabase/plugins/
+jar -jar /path/to/metabase/metabase.jar
+```
+
+_or:_
+
+```bash
+mkdir -p /path/to/metabase_source/plugins
+cp target/uberjar/http.metabase-driver.jar /path/to/metabase_source/plugins/
+cd /path/to/metabase_source
+lein run
+```
---
+++ src/metabase/driver/http/query_processor.clj
@@ -0,0 +1,128 @@
+(ns metabase.driver.http.query-processor
+  (:refer-clojure :exclude [==])
+  (:require [cheshire.core :as json]
+            [clojure.walk :as walk]
+            [clj-http.client :as client])
+  (:import [com.jayway.jsonpath JsonPath Predicate]))
+
+(declare compile-expression compile-function)
+
+(defn json-path
+  [query body]
+  (JsonPath/read body query (into-array Predicate [])))
+
+(defn compile-function
+  [[operator & arguments]]
+  (case (keyword operator)
+    :count count
+    :sum   #(reduce + (map (compile-expression (first arguments)) %))
+    :float #(Float/parseFloat ((compile-expression (first arguments)) %))
+    (throw (Exception. (str "Unknown operator: " operator)))))
+
+(defn compile-expression
+  [expr]
+  (cond
+    (string? expr)  (partial json-path expr)
+    (number? expr)  (constantly expr)
+    (vector? expr)  (compile-function expr)
+    :else           (throw (Exception. (str "Unknown expression: " expr)))))
+
+(defn aggregate
+  [rows metrics breakouts]
+  (let [breakouts-fns (map compile-expression breakouts)
+        breakout-fn   (fn [row] (for [breakout breakouts-fns] (breakout row)))
+        metrics-fns   (map compile-expression metrics)]
+    (for [[breakout-key breakout-rows] (group-by breakout-fn rows)]
+      (concat breakout-key (for [metrics-fn metrics-fns]
+                             (metrics-fn breakout-rows))))))
+
+(defn extract-fields
+  [rows fields]
+  (let [fields-fns (map compile-expression fields)]
+    (for [row rows]
+      (for [field-fn fields-fns]
+        (field-fn row)))))
+
+(defn field-names
+  [fields]
+  (vec (for [field fields]
+         (if (string? field)
+           {:name field}
+           {:name (json/generate-string field)}))))
+
+(defn tile38-keys-query [rows respond]
+  (let [columns    [{:name "data"}]
+        result  (apply list (for [row rows] (list row)))]
+    (respond {:cols columns}
+             result)))
+
+(defn tile38-query [rows respond]
+  (let [columns    [{:name "data"}]
+        result  (apply list (for [row rows] (list row)))]
+    (respond {:cols columns}
+             result)))
+
+(defn api-query [query rows respond]
+  (let [fields        (or (:fields (:result query)) (keys (first rows)))
+        aggregations  (or (:aggregation (:result query)) [])
+        breakouts     (or (:breakout (:result query)) [])
+        raw           (and (= (count breakouts) 0) (= (count aggregations) 0))
+        columns       (if raw
+                        (field-names fields)
+                        (field-names (concat breakouts aggregations)))
+        result         (if raw
+                         (extract-fields rows fields)
+                         (aggregate rows aggregations breakouts))]
+    (println "columns" columns)
+    (println "result" result)
+    (respond {:cols columns}
+             result)))
+
+(defn execute-http-request [native-query respond]
+  (let [query         (if (string? (:query native-query))
+                        (json/parse-string (:query native-query) keyword)
+                        (:query native-query))
+        body          (if (:body query) (json/generate-string (:body query)))
+        result        (client/request {:method  (or (:method query) :get)
+                                       :url     (:url query)
+                                       :headers (:headers query)
+                                       :body    body
+                                       :accept  :json
+                                       :as      :json
+                                       :content-type "application/json"})
+        rows-path     (or (:path (:result query)) "$")
+        rows          (json-path rows-path (walk/stringify-keys (:body result)))
+        from          (if (string? (:from query)) (:from query) "api")
+        is-tile38     (compare from "tile38")
+        is-tile38-keys  (if body (clojure.string/includes? body "KEYS") false)]
+    (println "rows" rows)
+    (cond
+      (= is-tile38 0) (tile38-query rows respond)
+      (= is-tile38-keys true) (tile38-keys-query rows respond)
+      :else (api-query query rows respond))))
+
+; (defn execute-http-request [native-query respond]
+;   (let [query         (if (string? (:query native-query))
+;                         (json/parse-string (:query native-query) keyword)
+;                         (:query native-query))
+;         result        (client/request {:method  (or (:method query) :get)
+;                                        :url     (:url query)
+;                                        :headers (:headers query)
+;                                        :body    (if (:body query) (json/generate-string (:body query)))
+;                                        :accept  :json
+;                                        :as      :json
+;                                        :content-type "application/json"})
+;         rows-path     (or (:path (:result query)) "$")
+;         rows          (json-path rows-path (walk/stringify-keys (:body result)))
+;         fields        (or (:fields (:result query)) (keys (first rows)))
+;         aggregations  (or (:aggregation (:result query)) [])
+;         breakouts     (or (:breakout (:result query)) [])
+;         raw           (and (= (count breakouts) 0) (= (count aggregations) 0))
+;         columns       (if raw
+;                         (field-names fields)
+;                         (field-names (concat breakouts aggregations)))
+;         result         (if raw
+;                          (extract-fields rows fields)
+;                          (aggregate rows aggregations breakouts))]
+;     (respond {:cols columns}
+;              result)))
